07516dd52b307faea968f3107691c6b2a4846c90
miyu210429 <k.miyu210429@gmail.com>
2025-08-19 22:25:41 +0900

    TaskListクラスの実装 実装内容は 対象となるtemplate要素の取得と、クローン作成、クローンした要素の子要素を操作して情報を追加、その要素をホスト要素にマウントです

--- src/kanban.ts
+++ src/kanban.ts
@@ -1,6 +1,9 @@
 console.log('コンパイル読み込み成功！');
 import { bound } from "./decorator/bindThis.js";
 
+const TASK_STATUS = ["todo", "working", "done"] as const; 
+type TaskStatus = (typeof TASK_STATUS)[number];
+
 interface Task {
     title: string;
     description?: string;
@@ -45,6 +48,26 @@ class TaskForm {
 
     private bindEvents() {
         this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+}
+
+class TaskList {
+    templateEl: HTMLTemplateElement;
+    element: HTMLDivElement;
+    private taskStatus: TaskStatus;
+
+    constructor(templateId: string, _taskStatus: TaskStatus) {
+        this.templateEl = document.querySelector(templateId)!;
+        const clone = this.templateEl.content.cloneNode(true) as DocumentFragment;
+        this.element = clone.firstElementChild as HTMLDivElement;
+        this.taskStatus = _taskStatus;
+
+        this.setup();
+    }
+
+    setup() {
+        this.element.querySelector('h2')!.textContent = `${this.taskStatus}`;
+        this.element.querySelector('ul')!.id = `${this.taskStatus}`;
     }
 }
 
