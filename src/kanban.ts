c5edfa1e02d85c67dad5f4a92147c2a6c0497b8a
miyu210429 <k.miyu210429@gmail.com>
2025-08-19 21:58:15 +0900

    bindThis.tsでデコレーターを使用。（thisを束縛するため） タスク入力フォームについての処理をkanban.tsに記載

--- src/kanban.ts
+++ src/kanban.ts
@@ -1,1 +1,51 @@
 console.log('コンパイル読み込み成功！');
+import { bound } from "./decorator/bindThis.js";
+
+interface Task {
+    title: string;
+    description?: string;
+}
+
+
+
+class TaskForm {
+    element: HTMLFormElement;
+    titleInputEl: HTMLInputElement;
+    descriptionInputEl: HTMLTextAreaElement;
+
+    constructor(){
+        this.element = document.querySelector('#task-form')!;
+        this.titleInputEl = document.querySelector('#form-title')!;
+        this.descriptionInputEl = document.querySelector('#form-description')!;
+
+        this.bindEvents();
+    }
+
+    private makeNewTask(): Task {
+        return {
+            title: this.titleInputEl.value,
+            description: this.descriptionInputEl.value,
+        };
+    }
+
+    private clearInputs(): void {
+        this.titleInputEl.value = '';
+        this.descriptionInputEl.value = '';
+    }
+
+    @bound
+    private submitHandler(event: Event) {
+        event.preventDefault();
+
+        console.log(this.titleInputEl.value);
+        console.log(this.descriptionInputEl.value);
+
+        this.clearInputs();
+    }
+
+    private bindEvents() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+}
+
+new TaskForm();
